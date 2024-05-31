f068570099a1dbe7bc6fd118649ed745b52398c5
Anudeep <anudeep.roy@liferay.com>
2024-05-31 14:38:47 +0530

    Added Modal

--- src/components/table-row.tsx
+++ src/components/table-row.tsx
@@ -1,5 +1,6 @@
 import data from '../elements.json';
 import Metal from './metal';
+import Modal from './modal';
 
 export default function TableRow () {
     return (
@@ -7,6 +8,7 @@ export default function TableRow () {
             {data.map((element, i)=>(
                 <Metal atomNum={i+1} name={element.name} symbol={element.symbol} background={element['cpk-hex']} row={element.period} column={element.group} standardState={element.category} atomicMass={element.atomic_mass}  boilingPoint={element.boil} description={element.summary}/>
             ))}
+            <Modal/>
         </div>
     )
 }
